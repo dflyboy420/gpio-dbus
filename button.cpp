f29faf0b87ec82501186dc31b621f8fd89c1b353
Daniel Poles <daniel@dpulm.online>
2021-06-02 23:15:29 +0200

    ase lambda

--- button.cpp
+++ button.cpp
@@ -35,40 +35,38 @@ int main(int argc, char *argv[])
   }
   else
   {
-    // button.f_pushed = [&]() { pushed(); };
-    while (button.read() == 0)
+    button.f_pushed = [&]()
     {
-      std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
+      if (nullptr ==
+          (dbus_msg = ::dbus_message_new_method_call(
+               "org.freedesktop.DBus", "/",
+               "org.freedesktop.DBus.Introspectable", "Introspect")))
+      {
+        ::dbus_connection_unref(dbus_conn);
+        ::perror("ERROR: ::dbus_message_new_method_call - Unable to allocate "
+                 "memory for the message!");
+      }
 
-    if (nullptr ==
-        (dbus_msg = ::dbus_message_new_method_call(
-             "org.freedesktop.DBus", "/",
-             "org.freedesktop.DBus.Introspectable", "Introspect")))
-    {
-      ::dbus_connection_unref(dbus_conn);
-      ::perror("ERROR: ::dbus_message_new_method_call - Unable to allocate "
-               "memory for the message!");
-    }
+      std::cout << "Connected to D-Bus as \""
+                << ::dbus_bus_get_unique_name(dbus_conn) << "\"." << std::endl;
+      std::cout << "Introspection Result:" << std::endl;
+      std::cout << std::endl
+                << dbus_result << std::endl
+                << std::endl;
+      ::dbus_message_unref(dbus_msg);
+      ::dbus_message_unref(dbus_reply);
 
-    std::cout << "Connected to D-Bus as \""
-              << ::dbus_bus_get_unique_name(dbus_conn) << "\"." << std::endl;
-    std::cout << "Introspection Result:" << std::endl;
-    std::cout << std::endl
-              << dbus_result << std::endl
-              << std::endl;
-    ::dbus_message_unref(dbus_msg);
-    ::dbus_message_unref(dbus_reply);
-
-    /*
+      /*
      * Applications must not close shared connections -
      * see dbus_connection_close() docs. This is a bug in the application.
      */
-    //::dbus_connection_close(dbus_conn);
+      //::dbus_connection_close(dbus_conn);
 
-    // When using the System Bus, unreference
-    // the connection instead of closing it
-    ::dbus_connection_unref(dbus_conn);
+      // When using the System Bus, unreference
+      // the connection instead of closing it
+      ::dbus_connection_unref(dbus_conn);
+    };
+    std::this_thread::sleep_for(std::chrono::minutes(1));
   }
 
   return 0;
