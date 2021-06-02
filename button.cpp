252a0d4db9822d2c39a5c27f6df26b3066145f98
Daniel Poles <daniel@dpulm.online>
2021-06-02 23:03:41 +0200

    send on btn press

--- button.cpp
+++ button.cpp
@@ -10,7 +10,8 @@
 using namespace GPIO;
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   (void)argc;
   (void)argv;
   DBusError dbus_error;
@@ -19,48 +20,44 @@ int main(int argc, char *argv[]) {
   DBusMessage *dbus_reply = nullptr;
   const char *dbus_result = nullptr;
 
+  DigitalIn button(15);
+
   // Initialize D-Bus error
   ::dbus_error_init(&dbus_error);
 
   // Connect to D-Bus
-  if (nullptr == (dbus_conn = ::dbus_bus_get(DBUS_BUS_SYSTEM, &dbus_error))) {
+  if (nullptr == (dbus_conn = ::dbus_bus_get(DBUS_BUS_SYSTEM, &dbus_error)))
+  {
     ::perror(dbus_error.name);
     ::perror(dbus_error.message);
 
     // Compose remote procedure call
-  } else if (nullptr ==
-             (dbus_msg = ::dbus_message_new_method_call(
-                  "org.freedesktop.DBus", "/",
-                  "org.freedesktop.DBus.Introspectable", "Introspect"))) {
-    ::dbus_connection_unref(dbus_conn);
-    ::perror("ERROR: ::dbus_message_new_method_call - Unable to allocate "
-             "memory for the message!");
+  }
+  else
+  {
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    std::chrono::milliseconds running;
+    while (button.get_state() < 1)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 
-    // Invoke remote procedure call, block for response
-  } else if (nullptr ==
-             (dbus_reply = ::dbus_connection_send_with_reply_and_block(
-                  dbus_conn, dbus_msg, DBUS_TIMEOUT_USE_DEFAULT,
-                  &dbus_error))) {
-    ::dbus_message_unref(dbus_msg);
-    ::dbus_connection_unref(dbus_conn);
-    ::perror(dbus_error.name);
-    ::perror(dbus_error.message);
+    if (nullptr ==
+        (dbus_msg = ::dbus_message_new_method_call(
+             "org.freedesktop.DBus", "/",
+             "org.freedesktop.DBus.Introspectable", "Introspect")))
+    {
+      ::dbus_connection_unref(dbus_conn);
+      ::perror("ERROR: ::dbus_message_new_method_call - Unable to allocate "
+               "memory for the message!");
+    }
 
-    // Parse response
-  } else if (!::dbus_message_get_args(dbus_reply, &dbus_error, DBUS_TYPE_STRING,
-                                      &dbus_result, DBUS_TYPE_INVALID)) {
-    ::dbus_message_unref(dbus_msg);
-    ::dbus_message_unref(dbus_reply);
-    ::dbus_connection_unref(dbus_conn);
-    ::perror(dbus_error.name);
-    ::perror(dbus_error.message);
-
-    // Work with the results of the remote procedure call
-  } else {
     std::cout << "Connected to D-Bus as \""
               << ::dbus_bus_get_unique_name(dbus_conn) << "\"." << std::endl;
     std::cout << "Introspection Result:" << std::endl;
-    std::cout << std::endl << dbus_result << std::endl << std::endl;
+    std::cout << std::endl
+              << dbus_result << std::endl
+              << std::endl;
     ::dbus_message_unref(dbus_msg);
     ::dbus_message_unref(dbus_reply);
 
